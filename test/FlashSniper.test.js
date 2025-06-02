3648ab44cfc617201d3769f18fb4812c93469508
Aljaž <aljaz.francic@gmail.com>
2025-06-02 06:00:32 +0200

    Patch

--- test/FlashSniper.test.js
+++ test/FlashSniper.test.js
@@ -1,24 +1,18 @@
-const FlashSniper = artifacts.require("FlashSniper");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-contract("FlashSniper", (accounts) => {
-  let instance;
-  const owner = accounts[0];
-  
-  beforeEach(async () => {
-    instance = await FlashSniper.new("0xD99D1c33F9fC3444f8101754aBC46c52416550D1", { from: owner });
+describe("FlashSniper", function () {
+  let flashSniper;
+  let owner;
+
+  beforeEach(async function () {
+    [owner] = await ethers.getSigners();
+    const FlashSniper = await ethers.getContractFactory("FlashSniper");
+    flashSniper = await FlashSniper.deploy();
+    await flashSniper.deployed();
   });
 
-  it("should set correct owner", async () => {
-    const executor = await instance.executor();
-    assert.equal(executor, owner, "Owner not set correctly");
-  });
-
-  it("should reject non-owner snipes", async () => {
-    try {
-      await instance.snipe("0x0000000000000000000000000000000000000000", 1000, { from: accounts[1] });
-      assert.fail("Should have thrown error");
-    } catch (error) {
-      assert.include(error.message, "Not executor", "Failed to reject non-owner");
-    }
+  it("should deploy correctly", async function () {
+    expect(await flashSniper.owner()).to.equal(owner.address);
   });
 });
