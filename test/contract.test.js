35cae3c46bf28ca441649a0e51ee0840b9ce8139
Aljaž <aljaz.francic@gmail.com>
2025-06-13 04:30:42 +0200

    Fixes

--- test/contract.test.js
+++ test/contract.test.js
@@ -10,46 +10,74 @@ describe("Sniper Contract", function () {
         // Deploy mock WETH
         const MockWETH = await ethers.getContractFactory("MockWETH");
         const weth = await MockWETH.deploy();
+        await weth.waitForDeployment();
+        const wethAddress = await weth.getAddress();
         
         // Deploy mock token
         const MockERC20 = await ethers.getContractFactory("MockERC20");
-        const token = await MockERC20.deploy("Test Token", "TEST", ethers.utils.parseEther("1000000"));
+        const token = await MockERC20.deploy("Test Token", "TEST", ethers.parseEther("1000000"));
+        await token.waitForDeployment();
+        const tokenAddress = await token.getAddress();
         
         // Deploy mock pair
         const MockPair = await ethers.getContractFactory("MockUniswapV2Pair");
-        const reserve0 = ethers.utils.parseEther("10000");
-        const reserve1 = ethers.utils.parseEther("10");
-        const pair = await MockPair.deploy(token.address, weth.address, reserve0, reserve1);
+        const reserve0 = ethers.parseEther("10000");
+        const reserve1 = ethers.parseEther("10");
+        const pair = await MockPair.deploy(tokenAddress, wethAddress, reserve0, reserve1);
+        await pair.waitForDeployment();
+        const pairAddress = await pair.getAddress();
 
         // Deploy mock factory
         const MockFactory = await ethers.getContractFactory("MockUniswapV2Factory");
-        const factory = await MockFactory.deploy(pair.address);
+        const factory = await MockFactory.deploy(pairAddress);
+        await factory.waitForDeployment();
+        const factoryAddress = await factory.getAddress();
 
         // Deploy mock router
         const MockRouter = await ethers.getContractFactory("MockUniswapV2Router");
-        const router = await MockRouter.deploy(weth.address);
-        await router.setFactory(factory.address);
+        const router = await MockRouter.deploy(wethAddress);
+        await router.waitForDeployment();
+        const routerAddress = await router.getAddress();
+        await router.setFactory(factoryAddress);
         
         // Deploy sniper contract
         const Sniper = await ethers.getContractFactory("Sniper");
-        const sniper = await Sniper.deploy(router.address);
+        const sniper = await Sniper.deploy(routerAddress);
+        await sniper.waitForDeployment();
+        const sniperAddress = await sniper.getAddress();
         
         // Setup mock liquidity
-        await token.transfer(router.address, reserve0);
+        await token.transfer(routerAddress, reserve0);
         await weth.deposit({ value: reserve1 });
-        await weth.transfer(router.address, reserve1);
+        await weth.transfer(routerAddress, reserve1);
         
-        return { sniper, router, weth, token, owner, user1, user2, pair, factory };
+        return { 
+            sniper, 
+            router, 
+            weth, 
+            token, 
+            owner, 
+            user1, 
+            user2, 
+            pair, 
+            factory,
+            sniperAddress,
+            routerAddress,
+            wethAddress,
+            tokenAddress,
+            pairAddress,
+            factoryAddress
+        };
     }
     
     describe("Deployment", function () {
         it("Should set the correct WETH address", async function () {
-            const { sniper, weth } = await loadFixture(deployContractsFixture);
-            expect(await sniper.WETH()).to.equal(weth.address);
+            const { sniper, wethAddress } = await loadFixture(deployContractsFixture);
+            expect(await sniper.WETH()).to.equal(wethAddress);
         });
         it("Should set the correct owner", async function () {
             const { sniper, owner } = await loadFixture(deployContractsFixture);
-            expect(await sniper.owner()).to.equal(owner.address);
+            expect(await sniper.owner()).to.equal(await owner.getAddress());
         });
         it("Should set default slippage and deadline", async function () {
             const { sniper } = await loadFixture(deployContractsFixture);
@@ -59,51 +87,51 @@ describe("Sniper Contract", function () {
     });
     describe("Buy Token", function () {
         it("Should buy tokens successfully", async function () {
-            const { sniper, token } = await loadFixture(deployContractsFixture);
-            const buyAmount = ethers.utils.parseEther("1");
-            const minTokens = ethers.utils.parseEther("100");
+            const { sniper, token, tokenAddress } = await loadFixture(deployContractsFixture);
+            const buyAmount = ethers.parseEther("1");
+            const minTokens = ethers.parseEther("100");
             await expect(
-                sniper.buyToken(token.address, minTokens, { value: buyAmount })
+                sniper.buyToken(tokenAddress, minTokens, { value: buyAmount })
             ).to.emit(sniper, "TokenBought");
-            const balance = await token.balanceOf(sniper.address);
+            const balance = await token.balanceOf(await sniper.getAddress());
             expect(balance).to.be.gt(0);
         });
         it("Should revert if no ETH sent", async function () {
-            const { sniper, token } = await loadFixture(deployContractsFixture);
+            const { sniper, tokenAddress } = await loadFixture(deployContractsFixture);
             await expect(
-                sniper.buyToken(token.address, 0)
+                sniper.buyToken(tokenAddress, 0)
             ).to.be.revertedWith("No ETH sent");
         });
         it("Should revert if token is blacklisted", async function () {
-            const { sniper, token } = await loadFixture(deployContractsFixture);
-            await sniper.blacklistToken(token.address, true);
+            const { sniper, tokenAddress } = await loadFixture(deployContractsFixture);
+            await sniper.blacklistToken(tokenAddress, true);
             await expect(
-                sniper.buyToken(token.address, 0, { value: ethers.utils.parseEther("1") })
+                sniper.buyToken(tokenAddress, 0, { value: ethers.parseEther("1") })
             ).to.be.revertedWith("Token is blacklisted");
         });
         it("Should only allow owner to buy", async function () {
-            const { sniper, token, user1 } = await loadFixture(deployContractsFixture);
+            const { sniper, tokenAddress, user1 } = await loadFixture(deployContractsFixture);
             await expect(
-                sniper.connect(user1).buyToken(token.address, 0, { value: ethers.utils.parseEther("1") })
+                sniper.connect(user1).buyToken(tokenAddress, 0, { value: ethers.parseEther("1") })
             ).to.be.revertedWith("Ownable: caller is not the owner");
         });
     });
     describe("Sell Token", function () {
         it("Should sell tokens successfully", async function () {
-            const { sniper, token } = await loadFixture(deployContractsFixture);
-            await sniper.buyToken(token.address, 0, { value: ethers.utils.parseEther("1") });
-            const tokenBalance = await token.balanceOf(sniper.address);
+            const { sniper, token, tokenAddress } = await loadFixture(deployContractsFixture);
+            await sniper.buyToken(tokenAddress, 0, { value: ethers.parseEther("1") });
+            const tokenBalance = await token.balanceOf(await sniper.getAddress());
             expect(tokenBalance).to.be.gt(0);
             await expect(
-                sniper.sellToken(token.address, tokenBalance)
+                sniper.sellToken(tokenAddress, tokenBalance)
             ).to.emit(sniper, "TokenSold");
-            const ethBalance = await ethers.provider.getBalance(sniper.address);
+            const ethBalance = await ethers.provider.getBalance(await sniper.getAddress());
             expect(ethBalance).to.be.gte(0);
         });
         it("Should revert if insufficient token balance", async function () {
-            const { sniper, token } = await loadFixture(deployContractsFixture);
+            const { sniper, tokenAddress } = await loadFixture(deployContractsFixture);
             await expect(
-                sniper.sellToken(token.address, ethers.utils.parseEther("100"))
+                sniper.sellToken(tokenAddress, ethers.parseEther("100"))
             ).to.be.revertedWith("Insufficient token balance");
         });
     });
@@ -136,11 +164,11 @@ describe("Sniper Contract", function () {
     });
     describe("Blacklist", function () {
         it("Should blacklist and unblacklist tokens", async function () {
-            const { sniper, token } = await loadFixture(deployContractsFixture);
-            await sniper.blacklistToken(token.address, true);
-            expect(await sniper.blacklistedTokens(token.address)).to.be.true;
-            await sniper.blacklistToken(token.address, false);
-            expect(await sniper.blacklistedTokens(token.address)).to.be.false;
+            const { sniper, tokenAddress } = await loadFixture(deployContractsFixture);
+            await sniper.blacklistToken(tokenAddress, true);
+            expect(await sniper.blacklistedTokens(tokenAddress)).to.be.true;
+            await sniper.blacklistToken(tokenAddress, false);
+            expect(await sniper.blacklistedTokens(tokenAddress)).to.be.false;
         });
     });
     describe("Withdrawals", function () {
@@ -148,25 +176,25 @@ describe("Sniper Contract", function () {
             const { sniper, owner } = await loadFixture(deployContractsFixture);
             // Send ETH to contract
             await owner.sendTransaction({
-                to: sniper.address,
-                value: ethers.utils.parseEther("1")
+                to: await sniper.getAddress(),
+                value: ethers.parseEther("1")
             });
-            const prevBalance = await ethers.provider.getBalance(owner.address);
+            const prevBalance = await ethers.provider.getBalance(await owner.getAddress());
             const tx = await sniper.withdrawETH();
             const receipt = await tx.wait();
             // Check that owner's balance increased by at least 1 ETH minus gas
-            const newBalance = await ethers.provider.getBalance(owner.address);
+            const newBalance = await ethers.provider.getBalance(await owner.getAddress());
             expect(newBalance).to.be.gt(prevBalance); // Can't check exact due to gas
         });
         it("Should withdraw tokens", async function () {
-            const { sniper, token, owner } = await loadFixture(deployContractsFixture);
-            await sniper.buyToken(token.address, 0, { value: ethers.utils.parseEther("1") });
-            const contractBalance = await token.balanceOf(sniper.address);
+            const { sniper, token, tokenAddress, owner } = await loadFixture(deployContractsFixture);
+            await sniper.buyToken(tokenAddress, 0, { value: ethers.parseEther("1") });
+            const contractBalance = await token.balanceOf(await sniper.getAddress());
             expect(contractBalance).to.be.gt(0);
-            const prevOwnerBalance = await token.balanceOf(owner.address);
-            await sniper.withdrawToken(token.address, contractBalance);
-            const ownerBalance = await token.balanceOf(owner.address);
-            expect(ownerBalance.sub(prevOwnerBalance)).to.equal(contractBalance);
+            const prevOwnerBalance = await token.balanceOf(await owner.getAddress());
+            await sniper.withdrawToken(tokenAddress, contractBalance);
+            const ownerBalance = await token.balanceOf(await owner.getAddress());
+            expect(ownerBalance - prevOwnerBalance).to.equal(contractBalance);
         });
         it("Should only allow owner to withdraw", async function () {
             const { sniper, user1 } = await loadFixture(deployContractsFixture);
@@ -177,9 +205,9 @@ describe("Sniper Contract", function () {
     });
     describe("Pair Info", function () {
         it("Should get pair info", async function () {
-            const { sniper, token, pair } = await loadFixture(deployContractsFixture);
-            const [pairAddr, tokenReserve, wethReserve, price] = await sniper.getPairInfo(token.address);
-            expect(pairAddr).to.equal(pair.address);
+            const { sniper, tokenAddress, pairAddress } = await loadFixture(deployContractsFixture);
+            const [pairAddr, tokenReserve, wethReserve, price] = await sniper.getPairInfo(tokenAddress);
+            expect(pairAddr).to.equal(pairAddress);
             expect(tokenReserve).to.be.gt(0);
             expect(wethReserve).to.be.gt(0);
             expect(price).to.be.gt(0);
